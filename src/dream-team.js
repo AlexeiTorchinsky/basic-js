c64fb6d9b5ff392fdc8f26c7f161cfe6a16134d9
Alexei Torchinsky <alextor1005@gmail.com>
2022-10-11 02:25:36 +0300

    feat: done  carbon dating, count-cats, dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let membersNew = [];
+  for (let member of members) {
+    if (typeof member === 'string') {
+      membersNew.push(member.trim());
+    }
+  }
+  const membersNewMapped = membersNew.map(el => el.substring(0, 1).toUpperCase()).sort();
+  return membersNewMapped.join('');
 }
 
 module.exports = {
